70aba325b533e743a8d69f8fbfc1e6d6f1f30d8e
Aleksander <Zakharov.owl@gmail.com>
2020-03-09 20:25:32 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = bracketsConfig.map( (item) => item.join("") );
+    //console.log(brackets);
+
+    let bracketsStr = str.split('');
+    for(let i = 0; i<bracketsStr.length;i++){
+        for(let a = 0; a<bracketsConfig.length; a++){
+            //console.log(brackets[a][1]);
+            //console.log(bracketsStr[i+1]);
+            
+            if(bracketsStr[i] === brackets[a][0] && bracketsStr[i+1] === brackets[a][1]){
+                bracketsStr.splice(i, 2);
+                i=-1;
+            } 
+        }
+    }
+    return bracketsStr.length === 0 ? true : false;
 }
